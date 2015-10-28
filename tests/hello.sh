6e02686ad45b8012341d345ea55bc4500a92a7e3
Andrei Barbu <andrei@0xab.com>
2015-10-28 14:41:00 -0400

    switch tests over to the new image

--- tests/hello.sh
+++ tests/hello.sh
@@ -3,11 +3,11 @@
 set -euf -o pipefail
 
 echo "tns create"
-docker run -it --privileged --net=host -v $PWD:/src abarbu/nativescript tns create hello
+docker run -it --privileged --net=host -v $PWD:/src abarbu/nativescript:1.4 tns create hello
 
 cd hello
 echo "tns platform add"
-docker run -it --privileged --net=host -v $PWD:/src abarbu/nativescript tns platform add android
+docker run -it --privileged --net=host -v $PWD:/src abarbu/nativescript:1.4 tns platform add android
 
 echo "ghcjs exists"
 docker run -it -v $PWD:/src abarbu/stack-ghcjs-nativescript:lts-3.0 ghcjs --help
